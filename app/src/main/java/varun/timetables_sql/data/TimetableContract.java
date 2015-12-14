1f046e6b6a9c0fbbc2e4f140b42e4c502ac3b1e1
Varun-garg <varun.10@live.com>
2015-12-14 23:55:05 +0530

    initial provider ready for testing

--- app/src/main/java/varun/timetables_sql/data/TimetableContract.java
+++ app/src/main/java/varun/timetables_sql/data/TimetableContract.java
@@ -1,6 +1,7 @@
 package varun.timetables_sql.data;
 
 import android.content.ContentResolver;
+import android.net.Uri;
 
 import varun.timetables_sql.MainActivity;
 
@@ -10,10 +11,49 @@ import varun.timetables_sql.MainActivity;
  */
 public class TimetableContract {
     public static String CONTENT_AUTHORITY = MainActivity.PACKAGE_NAME;
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
     public static String PATH_TIMETABLE = "tt";
     public static String PATH_SECTION = "section";
     public static String PATH_FACULTY = "faculty";
 
+    public static String PARAM_SLOT = "slot";
+    public static String PARAM_DAY = "day";
+
+    public static final Uri TT_CONTENT_URI =
+            BASE_CONTENT_URI.buildUpon().appendPath(PATH_TIMETABLE).build();
+
     public static String TT_CELL_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + PATH_TIMETABLE;
+
+    public static Uri BuildTTCellWithFacultyDaySlot(long fac_id, long day,long slot)
+    {
+        return TT_CONTENT_URI.buildUpon().appendPath(PATH_FACULTY).appendPath(Long.toString(fac_id))
+                .appendQueryParameter(PARAM_DAY, Long.toString(day))
+                .appendQueryParameter(PARAM_SLOT, Long.toString(slot)).build();
+    }
+    public static Uri BuildTTCellWithSectionDaySlot(long section_id, long day,long slot)
+    {
+        return TT_CONTENT_URI.buildUpon().appendPath(PATH_SECTION).appendPath(Long.toString(section_id))
+                .appendQueryParameter(PARAM_DAY, Long.toString(day))
+                .appendQueryParameter(PARAM_SLOT, Long.toString(slot)).build();
+    }
+    public static long getFacultyFromUri(Uri uri)
+    {
+        return Long.parseLong(uri.getPathSegments().get(2));
+    }
+    public static long getSectionFromUri(Uri uri)
+    {
+        return Long.parseLong(uri.getPathSegments().get(2));
+    }
+    public static long getSlotFromUri(Uri uri)
+    {
+        return Long.parseLong(uri.getQueryParameter(PARAM_SLOT));
+    }
+
+    public static long getDayFromUri(Uri uri)
+    {
+        return Long.parseLong(uri.getQueryParameter(PARAM_DAY));
+    }
+
 
 }
--- app/src/main/java/varun/timetables_sql/data/TimetableProvider.java
+++ app/src/main/java/varun/timetables_sql/data/TimetableProvider.java
@@ -13,36 +13,31 @@ import android.net.Uri;
  * varun.timetables_sql.data (Timetables_sql)
  * Created by Varun garg <varun.10@live.com> on 12/14/2015 7:44 PM.
  */
-public class TimetableProvider extends ContentProvider
-{
+public class TimetableProvider extends ContentProvider {
     private static final UriMatcher sUriMatcher = buildUriMatcher();
     private TimetableDbHelper mOpenHeler;
 
     static final int CELL_BY_SECTION_DAY_SLOT = 100;
     static final int CELL_BY_FACULTY_DAY_SLOT = 101;
 
-    static UriMatcher buildUriMatcher()
-    {
+    static UriMatcher buildUriMatcher() {
         final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
         final String authority = TimetableContract.CONTENT_AUTHORITY;
-        matcher.addURI(authority,TimetableContract.PATH_TIMETABLE + "/" + TimetableContract.PATH_SECTION,CELL_BY_SECTION_DAY_SLOT);
-        matcher.addURI(authority,TimetableContract.PATH_TIMETABLE + "/" + TimetableContract.PATH_FACULTY,CELL_BY_FACULTY_DAY_SLOT);
+        matcher.addURI(authority, TimetableContract.PATH_TIMETABLE + "/" + TimetableContract.PATH_SECTION + "/*", CELL_BY_SECTION_DAY_SLOT);
+        matcher.addURI(authority, TimetableContract.PATH_TIMETABLE + "/" + TimetableContract.PATH_FACULTY + "/*", CELL_BY_FACULTY_DAY_SLOT);
         return matcher;
     }
 
-    public boolean onCreate()
-    {
+    public boolean onCreate() {
         mOpenHeler = new TimetableDbHelper(getContext());
         return true;
     }
 
     @Override
-    public String getType(Uri uri)
-    {
+    public String getType(Uri uri) {
         final int match = sUriMatcher.match(uri);
 
-        switch (match)
-        {
+        switch (match) {
             case CELL_BY_SECTION_DAY_SLOT:
                 return TimetableContract.TT_CELL_TYPE;
             case CELL_BY_FACULTY_DAY_SLOT:
@@ -52,15 +47,57 @@ public class TimetableProvider extends ContentProvider
         }
     }
 
-    private cursor getTTCellBySection_id
+    private Cursor getTTCellBySectionDaySlot(Uri uri) {
+        Long section_id = TimetableContract.getSectionFromUri(uri);
+        Long day = TimetableContract.getDayFromUri(uri);
+        Long slot = TimetableContract.getSlotFromUri(uri);
 
+        String query = "SELECT ContGroupCode, CSF_Id,Room_Id, Batch_Id,ActivityTag FROM M_Time_Table Where Section_Id=" + section_id.toString() + " AND  TT_Day=" + day.toString() + " AND TT_Period=" + slot.toString();
+
+        return mOpenHeler.getReadableDatabase().rawQuery(query, null);
+    }
+
+    private Cursor getTTCellByFacultyDaySlot(Uri uri) {
+        Long faculty_id = TimetableContract.getFacultyFromUri(uri);
+        Long day = TimetableContract.getDayFromUri(uri);
+        Long slot = TimetableContract.getSlotFromUri(uri);
+
+        String query = "SELECT ContGroupCode, M_Time_Table.CSF_Id,Room_Id, Batch_Id,ActivityTag FROM M_Time_Table " +
+                " JOIN CSF_Faculty on M_Time_Table.CSF_Id=CSF_Faculty.csf_id Where faculty_Id=" + faculty_id + " AND  TT_Day=" + day + " AND TT_Period=" + slot;
+
+        return mOpenHeler.getReadableDatabase().rawQuery(query, null);
+    }
 
     @Override
-    public Cursor query(Uri uri, String[] projection,String selection,String[] SelectionArgs,String sortOrder)
-    {
+    public Cursor query(Uri uri, String[] projection, String selection, String[] SelectionArgs, String sortOrder) {
         Cursor retCursor;
-
+        switch (sUriMatcher.match(uri)) {
+            case CELL_BY_FACULTY_DAY_SLOT:
+                retCursor = getTTCellByFacultyDaySlot(uri);
+                break;
+            case CELL_BY_SECTION_DAY_SLOT:
+                retCursor = getTTCellBySectionDaySlot(uri);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        retCursor.setNotificationUri(getContext().getContentResolver(), uri);
         return retCursor;
     }
 
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {//TODO
+        return 1;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) //TODO
+    {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) { //TODO
+        return 1;
+    }
 }
