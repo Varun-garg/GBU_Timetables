1a050cf251b30d57cbd791a94cb55ff2528d30ee
Ritik Channa <chnritik@gmail.com>
2017-12-27 14:54:13 +0530

    Added BroadcastReceiver to reset alarm manager on device boot.

---
+++ app/src/main/java/com/varun/gbu_timetables/BootReceiver.java
@@ -0,0 +1,37 @@
+package com.varun.gbu_timetables;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.SystemClock;
+import android.util.Log;
+
+import com.varun.gbu_timetables.service.UpdateDatabaseService;
+
+import java.util.Calendar;
+
+import static android.content.Context.ALARM_SERVICE;
+
+public class BootReceiver extends BroadcastReceiver {
+    AlarmManager alarmMgr;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+            Calendar calendar = Calendar.getInstance();
+
+            calendar.set(Calendar.HOUR_OF_DAY, 13); // For 1 PM or 2 PM
+            calendar.set(Calendar.MINUTE, 0);
+            calendar.set(Calendar.SECOND, 0);
+            PendingIntent pi = PendingIntent.getService(context.getApplicationContext(), 0,
+                    new Intent(context.getApplicationContext(), UpdateDatabaseService.class), PendingIntent.FLAG_UPDATE_CURRENT);
+            AlarmManager am = (AlarmManager) context.getSystemService(ALARM_SERVICE);
+            Log.d("Ritik", "onDestroy: Starting service");
+            am.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                    AlarmManager.INTERVAL_DAY, pi);
+
+        }
+    }
+}
