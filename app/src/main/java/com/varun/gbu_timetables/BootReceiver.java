7832a278e157e101d514de67dd2854fcc740c64b
Ritik Channa <chnritik@gmail.com>
2017-12-27 19:20:15 +0530

    Remove unused Package

--- app/src/main/java/com/varun/gbu_timetables/BootReceiver.java
+++ app/src/main/java/com/varun/gbu_timetables/BootReceiver.java
@@ -5,7 +5,6 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 import com.varun.gbu_timetables.service.UpdateDatabaseService;
 import java.util.Calendar;
