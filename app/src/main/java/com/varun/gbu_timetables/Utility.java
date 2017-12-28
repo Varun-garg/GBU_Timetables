98012bddd57d4f6d7fe1b31a6cb016a215288051
Ritik Channa <chnritik@gmail.com>
2017-12-29 01:30:27 +0530

    MyClass Feature Minor Fix

--- app/src/main/java/com/varun/gbu_timetables/Utility.java
+++ app/src/main/java/com/varun/gbu_timetables/Utility.java
@@ -281,14 +281,7 @@ public class Utility {
         }
     }
 
-    public static String getMyclass(Context context){
 
-
-        return "todo";
-    }
-    public static void setMyclass(Context context,String myclass){
-
-    }
 
 
 
